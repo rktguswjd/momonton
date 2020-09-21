ab3a8c87f69e2b29f919074a9fccbfde68a7585e
rktguswjd <010_cm@naver.com>
2020-09-21 20:24:04 +0900

    Getting the Weather part One Geolocation

---
+++ weather.js
@@ -0,0 +1,38 @@
+const API_KEY = "a561e6575c9ec8e8f97599b7fdd6552d";
+const COORDS = "coords;";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("cant acee");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+init();
